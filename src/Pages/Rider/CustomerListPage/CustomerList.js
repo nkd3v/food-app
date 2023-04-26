e6c210832625628f145a82bb2616f9d38d636560
podwish <64011273@kmitl.ac.th>
2023-04-26 18:43:35 +0700

    add more file

--- src/Pages/Rider/CustomerListPage/CustomerList.js
+++ src/Pages/Rider/CustomerListPage/CustomerList.js
@@ -1,6 +1,19 @@
+import CustomerData from '../../../Components/customerdata/CustomerData'
+import './CustomerList.css'
+
 const CustomerList = () => {
     return(
-        <h1>CustomerList</h1>
+        <div>
+            <h1>Customer List</h1>
+            <div className='list_container'>
+                <CustomerData />
+                <CustomerData />
+                <CustomerData />
+                <CustomerData />
+                <CustomerData />    
+                <CustomerData />
+            </div>
+        </div>
     ) 
   };
   
