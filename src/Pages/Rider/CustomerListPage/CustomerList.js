fa32fe9a6d40e74f65996962d77aaff7b948bcfe
podwish <64011273@kmitl.ac.th>
2023-04-26 19:42:34 +0700

    fixed see button

--- src/Pages/Rider/CustomerListPage/CustomerList.js
+++ src/Pages/Rider/CustomerListPage/CustomerList.js
@@ -3,9 +3,27 @@ import './CustomerList.css'
 
 const CustomerList = () => {
     return(
+
+        
         <div>
             <h1>Customer List</h1>
+            
             <div className='list_container'>
+            
+            
+            <div class="col-sm-2 col-md-2 col-lg-2 border">
+                <h4>No.</h4>   
+            </div>
+            <div class="col-sm-4 col-md-4 col-lg-4 border">
+                <h4>Restaurant</h4>   
+            </div>
+            <div class="col-sm-4 col-md-4 col-lg-4 border">
+                <h4>Destination</h4> 
+            </div>
+            <div class="col-sm-2 col-md-2 col-lg-2 border">
+                <h4>Description</h4> 
+            </div>
+            
                 <CustomerData />
                 <CustomerData />
                 <CustomerData />
